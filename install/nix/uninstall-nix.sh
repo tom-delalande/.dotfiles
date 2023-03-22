87bf9e10cfb6f4c05e204e1ff8126fb18236d659
Tom Delalande <80664757+thomas-delalande@users.noreply.github.com>
2023-03-22 16:34:48 +0800

    fix(install): update nix uninstall instructions

--- install/nix/uninstall-nix.sh
+++ install/nix/uninstall-nix.sh
@@ -1,3 +1,19 @@
-sudo rm -rf /nix/
-sudo rm -rf /etc/nix
-rm -rf ~/.nix-channels ~/.nix-defexpr ~/.nix-profile
+# TODO: Automate this
+# Instructions
+# https://nixos.org/manual/nix/stable/installation/installing-binary.html#macos
+
+sudo mv /etc/zshrc.backup-before-nix /etc/zshrc
+sudo mv /etc/bashrc.backup-before-nix /etc/bashrc
+
+sudo launchctl unload /Library/LaunchDaemons/org.nixos.nix-daemon.plist
+sudo rm /Library/LaunchDaemons/org.nixos.nix-daemon.plist
+sudo launchctl unload /Library/LaunchDaemons/org.nixos.darwin-store.plist
+sudo rm /Library/LaunchDaemons/org.nixos.darwin-store.plist
+
+sudo dscl . -delete /Groups/nixbld
+for u in $(sudo dscl . -list /Users | grep _nixbld); do sudo dscl . -delete /Users/$u; done
+
+
+sudo rm -rf /etc/nix /var/root/.nix-profile /var/root/.nix-defexpr /var/root/.nix-channels ~/.nix-profile ~/.nix-defexpr ~/.nix-channels
+
+sudo diskutil apfs deleteVolume /nix
