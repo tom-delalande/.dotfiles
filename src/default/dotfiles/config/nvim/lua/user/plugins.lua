73044a2822a4fb2ebd7cd7bd2065636d511c5ba3
Tom Delalande <80664757+thomas-delalande@users.noreply.github.com>
2023-07-28 08:53:31 +0800

    fix: tmux ignore relevante files

--- src/default/dotfiles/config/nvim/lua/user/plugins.lua
+++ src/default/dotfiles/config/nvim/lua/user/plugins.lua
@@ -172,8 +172,13 @@ require("lazy").setup({
         end
     },
 
-    "weirongxu/plantuml-previewer.vim",
-    "aklt/plantuml-syntax",
+    {
+        "weirongxu/plantuml-previewer.vim",
+        dependencies = {
+            "aklt/plantuml-syntax",
+            "tyru/open-browser.vim",
+        }
+    },
 
     -- Functional
     -- -- Git
@@ -221,20 +226,20 @@ require("lazy").setup({
         branch = 'v1.x',
         dependencies = {
             -- LSP Support
-            { 'neovim/nvim-lspconfig' }, -- Required
-            { 'williamboman/mason.nvim' }, -- Optional
+            { 'neovim/nvim-lspconfig' },             -- Required
+            { 'williamboman/mason.nvim' },           -- Optional
             { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
             -- Autocompletion
-            { 'hrsh7th/nvim-cmp' }, -- Required
-            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-            { 'hrsh7th/cmp-buffer' }, -- Optional
-            { 'hrsh7th/cmp-path' }, -- Optional
+            { 'hrsh7th/nvim-cmp' },         -- Required
+            { 'hrsh7th/cmp-nvim-lsp' },     -- Required
+            { 'hrsh7th/cmp-buffer' },       -- Optional
+            { 'hrsh7th/cmp-path' },         -- Optional
             { 'saadparwaiz1/cmp_luasnip' }, -- Optional
-            { 'hrsh7th/cmp-nvim-lua' }, -- Optional
+            { 'hrsh7th/cmp-nvim-lua' },     -- Optional
 
             -- Snippets
-            { 'L3MON4D3/LuaSnip' }, -- Required
+            { 'L3MON4D3/LuaSnip' },             -- Required
             { 'rafamadriz/friendly-snippets' }, -- Optional
         },
         config = function()
