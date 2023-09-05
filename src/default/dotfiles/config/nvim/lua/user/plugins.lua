7d880dc770730fb753cdd506fe52ce5c6e7ac86c
Tom Delalande <80664757+thomas-delalande@users.noreply.github.com>
2023-09-05 13:34:08 +0800

    fix(nvim): fix lualine

--- src/default/dotfiles/config/nvim/lua/user/plugins.lua
+++ src/default/dotfiles/config/nvim/lua/user/plugins.lua
@@ -12,6 +12,17 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+local colors = {
+    blue   = '#80a0ff',
+    cyan   = '#79dac8',
+    black  = '#1e1e2e',
+    white  = '#c6c6c6',
+    red    = '#ff5189',
+    violet = '#d183e8',
+    grey   = '#303030',
+}
+
+
 local telescope_options = {
     defaults = {
         vimgrep_arguments = {
@@ -153,15 +164,48 @@ require("lazy").setup({
             require("lualine").setup(
                 {
                     options = {
-                        component_separators = { left = '', right = '' },
+                        theme = {
+                            normal = {
+                                a = { fg = colors.black, bg = colors.violet },
+                                b = { fg = colors.white, bg = colors.grey },
+                                c = { fg = colors.black, bg = colors.black },
+                            },
+
+                            insert = { a = { fg = colors.black, bg = colors.blue } },
+                            visual = { a = { fg = colors.black, bg = colors.cyan } },
+                            replace = { a = { fg = colors.black, bg = colors.red } },
+
+                            inactive = {
+                                a = { fg = colors.white, bg = colors.black },
+                                b = { fg = colors.white, bg = colors.black },
+                                c = { fg = colors.black, bg = colors.black },
+                            },
+                        },
+                        component_separators = '|',
                         section_separators = { left = '', right = '' },
                     },
                     sections = {
-                        lualine_c = {
-                            'filename',
-                            'lsp_progress'
-                        }
-                    }
+                        lualine_a = {
+                            { 'mode', separator = { left = '' }, right_padding = 2 },
+                        },
+                        lualine_b = { 'filename', 'branch' },
+                        lualine_c = { 'fileformat' },
+                        lualine_x = {},
+                        lualine_y = { 'filetype', 'progress' },
+                        lualine_z = {
+                            { 'location', separator = { right = '' }, left_padding = 2 },
+                        },
+                    },
+                    inactive_sections = {
+                        lualine_a = { 'filename' },
+                        lualine_b = {},
+                        lualine_c = {},
+                        lualine_x = {},
+                        lualine_y = {},
+                        lualine_z = { 'location' },
+                    },
+                    tabline = {},
+                    extensions = {},
                 }
             )
         end,
