a9e57af81055da6fc09ee58cfd3c3a79900cb5d2
Tom Delalande <80664757+thomas-delalande@users.noreply.github.com>
2024-12-13 10:16:03 +0800

    feat(nvim): add dap dependencies

--- src/default/dotfiles/config/nvim/lua/user/plugins.lua
+++ src/default/dotfiles/config/nvim/lua/user/plugins.lua
@@ -129,8 +129,38 @@ require("lazy").setup({
                 "rcarriga/nvim-dap-ui",
                 "theHamsta/nvim-dap-virtual-text",
                 "nvim-neotest/nvim-nio",
+                "williamboman/mason.nvim",
             },
             config = function()
+                local dap = require("dap")
+                local ui = require("dapui")
+
+                ui.setup()
+
+                require("navim-dap-virtual-text").setup({
+                })
+                vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, {})
+                vim.keymap.set("n", "<leader>gb", dap.run_to_cursor, {})
+                vim.keymap.set("n", "<leader>k", ui.eval(nil, { enter = true }), {})
+                vim.keymap.set("n", "<leader>1", dap.continue, {})
+                vim.keymap.set("n", "<leader>2", dap.step_into, {})
+                vim.keymap.set("n", "<leader>3", dap.step_over, {})
+                vim.keymap.set("n", "<leader>4", dap.step_out, {})
+                vim.keymap.set("n", "<leader>5", dap.step_back, {})
+                vim.keymap.set("n", "<leader>0", dap.restart, {})
+
+                dap.listeners.before.attach.dapui_config = function()
+                    ui.open()
+                end
+                dap.listeners.before.launch.dapui_config = function()
+                    ui.open()
+                end
+                dap.listeners.before.event_terminated.dapui_config = function()
+                    ui.close()
+                end
+                dap.listeners.before.event_exited.dapui_config = function()
+                    ui.close()
+                end
             end
         },
 
