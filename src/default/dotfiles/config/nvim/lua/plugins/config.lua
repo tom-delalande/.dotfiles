734372060da1afed34daca6e8800c257f1ee7cf6
Tom Delalande <80664757+thomas-delalande@users.noreply.github.com>
2025-07-06 16:07:48 +0800

    fix: autocomplete

--- src/default/dotfiles/config/nvim/lua/plugins/config.lua
+++ src/default/dotfiles/config/nvim/lua/plugins/config.lua
@@ -12,4 +12,17 @@ return {
       },
     },
   },
+  {
+    "saghen/blink.cmp",
+    opts = {
+      completion = {
+        list = {
+          selection = {
+            preselect = false,
+            auto_insert = false,
+          },
+        },
+      },
+    },
+  },
 }
