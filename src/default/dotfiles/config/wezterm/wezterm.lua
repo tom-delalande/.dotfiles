21e7e47b77c33c9721e85156b48e3670c3f049a6
Tom Delalande <80664757+thomas-delalande@users.noreply.github.com>
2025-07-07 15:27:31 +0800

    wip

--- src/default/dotfiles/config/wezterm/wezterm.lua
+++ src/default/dotfiles/config/wezterm/wezterm.lua
@@ -5,7 +5,7 @@ return {
     font_size = 18.0,
     -- font_size = 24.0,
     enable_tab_bar = false,
-    window_decorations = "RESIZE",
+    window_decorations = "NONE",
     color_scheme = "Catppuccin Mocha",
     -- window_background_opacity = 0.3,
     -- macos_window_background_blur = 20
