b27b1d9627f8e3c9c6f48cf311a7232dc38dd955
Tom Delalande <80664757+thomas-delalande@users.noreply.github.com>
2025-07-07 19:52:45 +0800

    fix: add more extra plugins

--- src/default/dotfiles/config/nvim/lua/plugins/config.lua
+++ src/default/dotfiles/config/nvim/lua/plugins/config.lua
@@ -19,7 +19,7 @@ return {
         list = {
           selection = {
             preselect = false,
-            auto_insert = false,
+            -- auto_insert = false,
           },
         },
       },
