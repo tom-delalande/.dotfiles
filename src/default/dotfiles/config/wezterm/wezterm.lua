ca01df1a6ee1c0bd8ed72a9c85cb5dad6f0c92bb
Tom Delalande <80664757+thomas-delalande@users.noreply.github.com>
2025-07-08 14:20:11 +0800

    update

--- src/default/dotfiles/config/wezterm/wezterm.lua
+++ src/default/dotfiles/config/wezterm/wezterm.lua
@@ -1,12 +1,20 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 return {
-    font = wezterm.font('JetBrains Mono'),
-    font_size = 14.0,
-    -- font_size = 24.0,
-    enable_tab_bar = false,
-    window_decorations = "NONE",
-    color_scheme = "Catppuccin Mocha",
-    -- window_background_opacity = 0.3,
-    -- macos_window_background_blur = 20
+	font = wezterm.font("JetBrains Mono"),
+	font_size = 18.0,
+	-- font_size = 24.0,
+	enable_tab_bar = false,
+	window_decorations = "RESIZE",
+	color_scheme = "Catppuccin Mocha",
+	window_close_confirmation = "NeverPrompt",
+	-- window_background_opacity = 0.3,
+	-- macos_window_background_blur = 20
+	keys = {
+		{
+			key = "w",
+			mods = "CMD",
+			action = wezterm.action.CloseCurrentTab({ confirm = false }),
+		},
+	},
 }
